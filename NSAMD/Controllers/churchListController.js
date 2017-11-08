f757eeabb49cffba2d77e54d38335e364a4652f0
CoderVision <garymisc@outlook.com>
2017-11-07 20:33:26 -0800

    Wired up member church selector to config list

---
+++ NSAMD/Controllers/churchListController.js
@@ -0,0 +1,11 @@
+﻿
+'use strict';
+
+angular.module('app').controller('churchListController',
+    ['$mdDialog', '$mdMedia', '$mdBottomSheet', '$location', '$log', 'churchService'
+    , function ($mdDialog, $mdMedia, $mdBottomSheet, $location, $log, churchService) {
+
+        var vm = this;
+
+        return vm;
+    }]);
