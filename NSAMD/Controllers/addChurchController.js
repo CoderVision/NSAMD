a55848e591a248bbbdc8c83c933e36b5a80814dd
CoderVision <garymisc@outlook.com>
2017-11-10 22:25:26 -0800

    abstracted time zone list to configService

--- NSAMD/Controllers/addChurchController.js
+++ NSAMD/Controllers/addChurchController.js
@@ -2,25 +2,15 @@
 'use strict';
 
 angular.module('app')
-    .controller('addChurchController', ['$mdDialog', '$log', 'churchService', 'appNotificationService', 'currentItem', 'config'
-        , function ($mdDialog, $log, churchService, appNotificationService, currentItem, config) {
+    .controller('addChurchController', ['$mdDialog', '$log', 'churchService', 'appNotificationService', 'currentItem', 'config', 'configService'
+        , function ($mdDialog, $log, churchService, appNotificationService, currentItem, config, configService) {
 
             var vm = this;
             vm.config = config;
             vm.currentItem = currentItem;
 
             vm.load = function () {
-                vm.config.timeZones = [
-                  { name: "Samoa", offset: "-11" } // remove ending ":00" or Angular will throw errors.  We can add it below when we save.
-                , { name: "Hawaii", offset: "-10" }
-                , { name: "Alaska", offset: "-09" }
-                , { name: "Pacific", offset: "-08" }
-                , { name: "Mountain", offset: "-07" }
-                , { name: "Central", offset: "-06" }
-                , { name: "Eastern", offset: "-05" }
-                , { name: "Atlantic", offset: "-04" }
-                , { name: "Chamorro", offset: "+10" }
-                ];
+                vm.config = configService.getTimeZones();
             }
 
             vm.cancel = function () {
