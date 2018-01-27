e07ce238fcdb25fa6ca3443cb37246f807034355
Gary Lima <garylima@outlook.com>
2018-01-26 23:00:37 -0800

    Added admin user account request review, and settings tab.

--- NSAMD/Controllers/adminUsersController.js
+++ NSAMD/Controllers/adminUsersController.js
@@ -3,10 +3,28 @@
 'use strict';
 
 angular.module('app').controller('adminUsersController',
-    ['$mdDialog', '$mdMedia', '$mdBottomSheet', '$location', '$log'
-        , function ($mdDialog, $mdMedia, $mdBottomSheet, $location, $log) {
+    ['$mdDialog', '$mdMedia', '$mdBottomSheet', '$location', '$log', 'userService', 'appNotificationService'
+        , function ($mdDialog, $mdMedia, $mdBottomSheet, $location, $log, userService, appNotificationService) {
 
             var vm = this;
+
+            vm.message = "Hello from users controller!";
+            vm.acctRequests = [];
+            vm.config = {};
+            vm.users = [];
+
+            vm.load = function ()
+            {
+                userService.getList().then(function (success) {
+
+                    vm.config = success.config;
+                    vm.acctRequests = success.acctRequests;  
+                    vm.users = success.users;
+
+                }, function (error) {
+                    appNotificationService.openToast("Error loading member config:  " + error);
+                });
+            }
 
             return vm;
         }]);
---
+++ NSAMD/Services/userService.js
@@ -0,0 +1,79 @@
+﻿
+
+
+'use strict'
+
+angular.module('app').factory('userService', ['$http', '$log', '$q', 'config'
+    , function ($http, $log, $q, config) {
+
+        var svc = {};
+
+      
+        svc.getList = function () {
+
+            var deferred = $q.defer();
+
+            var uri = config.apiUrl + "/account/GetUsers";
+
+            $http.get(uri).then(function (success) {
+
+                deferred.resolve(success.data);
+
+            }, function (error) {
+
+                var err = error | err.message;
+
+                $log.error("error in memberService.getList:  " + err);
+
+                deferred.reject("error in memberService.getList:  " + err);
+            });
+
+            return deferred.promise;
+        };
+
+        //svc.patch = function (memberId, churchId, fieldName, fieldValue) {
+
+        //    var deferred = $q.defer();
+
+        //    var uri = config.apiUrl + "/Members?id=" + memberId + "&churchId=" + churchId;
+        //    var patchDocument = [{
+        //        "op": "replace",
+        //        "path": "/" + fieldName,
+        //        "value": fieldValue
+        //    }];
+
+        //    $http.patch(uri, patchDocument).then(function (success) {
+
+        //        deferred.resolve(success.data);
+
+        //    }, function (error) {
+
+        //        $log.error("error in memberService.get:  " + error | error.message);
+
+        //        deferred.reject("Error retrieving member id " + memberId);
+        //    });
+
+        //    return deferred.promise;
+        //}
+
+        //svc.save = function (addy) {
+        //    var deferred = $q.defer();
+
+        //    var uri = config.apiUrl + "/Members/" + addy.identityId + "/" + addy.type;
+
+        //    $http.post(uri, addy).then(function (success) {
+
+        //        deferred.resolve(success.data);
+
+        //    }, function (error) {
+
+        //        $log.error("error in memberService.saveAddy:  " + error);
+
+        //        deferred.reject("Error retrieving address id " + addy.id);
+        //    });
+
+        //    return deferred.promise;
+        //}
+
+        return svc;
+    }]);
