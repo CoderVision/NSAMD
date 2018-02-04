5b107c061354c271795e6d14c0e1fed6c1c38454
Gary Lima <garylima@outlook.com>
2018-02-03 20:41:31 -0800

    Added sms corresondence list and message history panel

--- NSAMD/Controllers/smsController.js
+++ NSAMD/Controllers/smsController.js
@@ -10,6 +10,58 @@ angular.module('app').controller('smsController',
             var vm = this;
 
             vm.message = "Hello from the smsController!";
-           
+            vm.correspondences = [];
+            vm.selectedItem = null;
+            vm.selectedIndex = 0;
+
+            vm.init = function () {
+                var correspondence1 = {
+                    id: 1,
+                    Name: 'Gary Lima',
+                    LastMessageDate: '01/27/2018',
+                    LastMessageText: 'Hi there!  this is a test.  :)',
+                    Read: true,                                                     // does this message have any unread messages?
+                    Messages: [{ id: 1, content:'Hi there!  this is a test.  :)', direction: 'Received'}]
+                };
+                var correspondence2 = {
+                    id: 2,
+                    Name: 'Wei Lima',
+                    LastMessageDate: '01/28/2018',
+                    LastMessageText: 'Hi!!!  Hope youre having fun!',
+                    Read: true,
+                };
+                var correspondence3 = {
+                    id: 3,
+                    Name: 'Jeff Malone',
+                    LastMessageDate: '02/01/2018',
+                    LastMessageText: 'Hows the site going?',
+                    Read: true
+                };
+                var correspondence4 = {
+                    id: 4,
+                    Name: 'David Bean',
+                    LastMessageDate: '02/02/2018',
+                    LastMessageText: 'Bless you sir!',
+                    Read: true
+                };
+                var correspondence5 = {
+                    id: 5,
+                    Name: 'Chris Hayes',
+                    LastMessageDate: '02/03/2018',
+                    LastMessageText: 'Just checking in',
+                    Read: false
+                };
+
+ 
+                vm.correspondences = [correspondence1, correspondence2, correspondence3, correspondence4, correspondence5];
+
+                vm.selectedItem = correspondence1;
+
+            }
+
+            vm.viewMessages = function (index, correspondence) {
+                vm.selectedIndex = index;
+                vm.selectedItem = correspondence;
+            }
             return vm;
         }]);
