ae0f579c1c068540e17398c1dd5edce21c66bb86
CoderVision <garymisc@outlook.com>
2018-03-12 21:53:51 -0700

    cleared selected item for sms

--- NSAMD/Controllers/smsController.js
+++ NSAMD/Controllers/smsController.js
@@ -36,10 +36,10 @@ angular.module('app').controller('smsController',
             // order the list when it changes and set it to the array
             $scope.$watch("smc.correspondences | orderBy : '-LastMessageDate'", function (newValue) {
 
-                if (vm.selectedItem === undefined) {
+                //if (vm.selectedItem === undefined) {
                     vm.correspondencesOrdered = newValue;
                     vm.viewMessages(0, vm.correspondencesOrdered[0]);
-                }
+               // }
             }, true);
 
             // message controller churchId
@@ -47,6 +47,7 @@ angular.module('app').controller('smsController',
                 if (newValue !== oldValue
                     && vm.churchId !== newValue) {
                     vm.churchId = newValue;
+                    vm.selectedItem = undefined;
                     vm.loadData();
                 }
             }, false);
@@ -69,49 +70,8 @@ angular.module('app').controller('smsController',
             }
 
             vm.init = function () {
-
+         
                 vm.loadData();
-
-                //var correspondence1 = {
-                //    id: 1,
-                //    Name: 'Gary Lima',
-                //    LastMessageDate: '01/27/2018',
-                //    LastMessageText: 'Hi there!  this is a test.  :)',
-                //    Read: true,                                                     // does this message have any unread messages?
-                //    messages: [{ id: 1, body: 'Hi there!  this is a test.  :)', directionEnumId: 53, messageDate: '2/9/18 6:45 PM' },
-                //    { id: 2, body: 'Yes!  I got your test  Let me know if you receive mine from NtccSteward.', directionEnumId: 54, messageDate: '2/9/18 6:46 PM' },
-                //    { id: 3, body: 'Yup, I got it!', directionEnumId: 53, messageDate: '2/9/18 6:47 PM' }]
-                //};
-                //var correspondence2 = {
-                //    id: 2,
-                //    Name: 'Wei Lima',
-                //    LastMessageDate: '01/28/2018',
-                //    LastMessageText: 'Hi!!!  Hope youre having fun!',
-                //    Read: true,
-                //};
-                //var correspondence3 = {
-                //    id: 3,
-                //    Name: 'Jeff Malone',
-                //    LastMessageDate: '02/01/2018',
-                //    LastMessageText: 'Hows the site going?',
-                //    Read: true
-                //};
-                //var correspondence4 = {
-                //    id: 4,
-                //    Name: 'David Bean',
-                //    LastMessageDate: '02/02/2018',
-                //    LastMessageText: 'Bless you sir!',
-                //    Read: true
-                //};
-                //var correspondence5 = {
-                //    id: 5,
-                //    Name: 'Chris Hayes',
-                //    LastMessageDate: '02/03/2018',
-                //    LastMessageText: 'Just checking in',
-                //    Read: false
-                //};
-
-                //vm.correspondences = [correspondence1, correspondence2, correspondence3, correspondence4, correspondence5];
             }
 
             vm.loadData = function () {
