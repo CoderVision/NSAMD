56ad00574e4aca278f829ae9039fc4fd0a58e094
CoderVision <garymisc@outlook.com>
2018-06-20 17:25:30 -0700

    Updated hiding of fields for list views (members, etc)

--- NSAMD/config.js
+++ NSAMD/config.js
@@ -4,7 +4,7 @@
 
     window.__config = window.__env || {};
 
-    window.__config.apiUrl = 'https://localhost:44352/';
+    window.__config.apiUrl = 'https://localhost:44352';
     //window.__config.apiUrl = 'https://www.hissteward.com/api';
 
     window.__config.stsUrl = 'https://localhost:44316/';
