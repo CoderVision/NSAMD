1a6a9fdbbadeb67428e0dbe22f160ddfb56707fd
CoderVision <garymisc@outlook.com>
2018-07-30 19:48:00 -0700

    Added scripts

--- gulp.config.js
+++ gulp.config.js
@@ -4,12 +4,25 @@ module.exports = function () {
     let config = {
         temp: './tmp/',
         alljs: [
-            src + '/**/*.js', // was src
-            './*.js'
+            src + '/config.js', 
+            src + '/app.js', 
+            src + '/Services/appService.js',
+            src + '/Services/appNotificationService.js',
+            src + '/Services/errorService.js',
+            src + '/Services/configService.js',
+            src + '/Services/churchService.js',
+            src + '/Services/memberService.js',
+            src + '/Services/teamService.js',
+            src + '/Services/reportService.js',
+            src + '/Services/authService.js',
+            src + '/Services/userService.js',
+            src + '/Services/messageService.js',
+            src + '/Controllers/**/*.js',
+            src + '/Filters/**/*.js'
         ],
         css: src + '/Content/app.css',
-        sass: src + '/Content/appSass.scss'
-
+        sass: src + '/Content/appSass.scss',
+        dist: './dist/'
     };
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,50 @@ var del = require('del');
 var uuid = require('uuid/v4');
 //var uglify = require('gulp-uglify'); // minify javascript:  $.uglify
 
+var print = require('gulp-print').default;
+
 var $ = require('gulp-load-plugins')({ lazy: true });
 // use $. with the name of the plugin without gulp- in front of it, e.g.:  $.print instead of gulp-print
 
-var gulpPrint = require('gulp-print');
 // .pipe(gulpPrint())  // see all of the files you are touching
-var gulpif = require('gulp-if');
-
+// var gulpif = require('gulp-if');
 //.pipe(gulpif(args.verbose, gulpPrint()))  // run gulp task with --verbose to trigger the gulpPrint
 
 
+gulp.task('build', ['scripts', 'libs', 'styles'], function () {
+    return gulp
+        .src(['NSAMD/**/*.html', config.temp + '/**/*.*'])
+        .pipe(print())
+        .pipe(gulp.dest(config.dist))
+})
+
+gulp.task('serve', ['build'], function () {
+    gulp.src('dist').pipe(webserver({ open: true }))
+})
 
 // add gulp-polyfill also
+gulp.task('scripts', ['clean-scripts'], function () {
+    log('Compiling Javascript');
+
+    return gulp
+        .src(config.alljs)
+        .pipe($.plumber())  // gracefully handles errors
+        .pipe(print())      // #2. print each file in the stream
+        .pipe($.concat(uuid() + '.js'))
+        .pipe($.babel({ presets: ['es2015'] })) // #3. transpile ES2015 to ES5 using ES2015 preset
+        .pipe($.uglify())
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('libs', function () {
+    return gulp
+        .src([
+            'node_modules/systemjs/dist/system.js',
+            'node_modules/babel-polyfill/dist/polyfill.js',
+        ])
+        .pipe(print())
+        .pipe(gulp.dest(config.dist + 'libs'))
+})
 
 gulp.task('styles', ['clean-styles'], function () {
     log('Compiling SASS -> CSS');
@@ -27,6 +59,7 @@ gulp.task('styles', ['clean-styles'], function () {
     return gulp
         .src([config.css, config.sass])
         .pipe($.plumber())  // gracefully handles errors
+        .pipe(print()) 
         .pipe($.concat(uuid() + '.scss'))
         .pipe($.sass())
         .pipe($.autoprefixer({ browsers: ['last 2 versions', '> 5%']})) // get only the last 2 versions of browsers that have more than 5% of the market.
@@ -37,6 +70,11 @@ gulp.task('styles', ['clean-styles'], function () {
 // this is a dependency of styles
 gulp.task('clean-styles', function () {
     var files = config.temp + '**/*.css';
+    clean(files);
+});
+
+gulp.task('clean-scripts', function () {
+    var files = config.temp + '**/*.js';
     clean(files);
 });
 
